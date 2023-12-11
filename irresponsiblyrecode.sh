ed324c766d33d11f84c18e6b01804e7e94797f1d
Riyan Gendut <149429145+riyan-gendut@users.noreply.github.com>
2023-12-11 15:28:00 +0700

    uploaded irresponsiblyrecode.sh
    a bash script to find every files in the active directory and transcode every compatible files into x265 HEVC mp4 video using ffmpeg

---
+++ irresponsiblyrecode.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+dewit(){
+#THIS WILL RECODE EVERY FILE IN CURRENT DIRECTORY AND ITS SUBDIRECTORY TO x265. USE AT YOUR OWN RISK.
+mkdir ~/vtout/
+find . -type f -name "*.mp4" -exec bash -c 'file="$1"; echo $(stat -c '%n' "$file") $(stat -c '%s' "$file"); ffmpeg -i "$file" -c:v libx265 -vtag hvc1 ~/vtout/"$file"' _ {} \;
+find . -type f -not -name "*.mp4" -exec bash -c 'file="$1"; echo $(stat -c '%n' "$file") $(stat -c '%s' "$file"); ffmpeg -i "$file" -c:v libx265 -vtag hvc1 ~/vtout/"$file".mp4' _ {} \;
+}
+
+read -p "Are you sure you're in the right directory? (y/n)" yn
+case $yn in
+	['y''Y'] ) dewit; exit;;
+	['n''N'] ) echo "Wise decision."; exit;;
+	* ) echo "Invalid."; exit;;
+esac
+
