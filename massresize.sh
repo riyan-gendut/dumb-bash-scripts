bb3d969396a225347d8287e777aacf965f08145b
Riyan Gendut <149429145+riyan-gendut@users.noreply.github.com>
2024-01-08 01:07:17 +0700

    Update massresize.sh
    escaping variables

--- massresize.sh
+++ massresize.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 mkdir ~/tmpimg
 for f in *.jpg ; do
-ffmpeg -i "$f" -vf scale="1000:1000:force_original_aspect_ratio=increase" "~/tmpimg/$f"
+ffmpeg -i "$f" -vf scale="1000:1000:force_original_aspect_ratio=increase" ~/tmpimg/"$f"
 done
 for f in *.jpeg ; do
-ffmpeg -i "$f" -vf scale="1000:1000:force_original_aspect_ratio=increase" "~/tmpimg/$f"
+ffmpeg -i "$f" -vf scale="1000:1000:force_original_aspect_ratio=increase" ~/tmpimg/"$f"
 done
 for f in *.png ; do
-ffmpeg -i "$f" -vf scale="1000:1000:force_original_aspect_ratio=increase" "~/tmpimg/$f"
+ffmpeg -i "$f" -vf scale="1000:1000:force_original_aspect_ratio=increase" ~/tmpimg/"$f"
 done
