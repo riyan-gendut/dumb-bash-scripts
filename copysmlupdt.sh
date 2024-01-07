6e46fba1f951c9bf5aecc90da761d97cb8e9232f
Riyan Gendut <149429145+riyan-gendut@users.noreply.github.com>
2024-01-08 01:33:34 +0700

    Update copysmlupdt.sh
    updating old var

--- copysmlupdt.sh
+++ copysmlupdt.sh
@@ -4,13 +4,13 @@ newsml=$2
 cd $oldlrg
 for f in *.jpg ; do
 echo $(stat -c '%n' "$f")
-rsync --max-size=$(stat -c '%s' "$f") "$newsml/$f" "$oldlrg/$file" && rm "$newsml/$file"
+rsync --max-size=$(stat -c '%s' "$f") "$newsml/$f" "$oldlrg/$f" && rm "$newsml/$f"
 done
 for f in *.jpeg ; do
 echo $(stat -c '%n' "$f")
-rsync --max-size=$(stat -c '%s' "$f") "$newsml/$f" "$oldlrg/$file" && rm "$newsml/$file"
+rsync --max-size=$(stat -c '%s' "$f") "$newsml/$f" "$oldlrg/$f" && rm "$newsml/$f"
 done
 for f in *.png ; do
 echo $(stat -c '%n' "$f")
-rsync --max-size=$(stat -c '%s' "$f") "$newsml/$f" "$oldlrg/$file" && rm "$newsml/$file"
+rsync --max-size=$(stat -c '%s' "$f") "$newsml/$f" "$oldlrg/$f" && rm "$newsml/$f"
 done
