403f0cb360fc0a4997d82b798d006e547d103416
Riyan Gendut <149429145+riyan-gendut@users.noreply.github.com>
2024-01-13 13:55:19 +0700

    realized I could do it way shorter

--- massresize.sh
+++ massresize.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 mkdir ~/tmpimg
-for f in *.jpg ; do
-ffmpeg -i "$f" -vf scale="1000:1000:force_original_aspect_ratio=increase" ~/tmpimg/"$f"
-done
-for f in *.jpeg ; do
-ffmpeg -i "$f" -vf scale="1000:1000:force_original_aspect_ratio=increase" ~/tmpimg/"$f"
-done
-for f in *.png ; do
+for f in *.jpg *.jpeg *.JPG *.png *.webp; do
 ffmpeg -i "$f" -vf scale="1000:1000:force_original_aspect_ratio=increase" ~/tmpimg/"$f"
 done
