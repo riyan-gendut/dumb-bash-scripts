196fb51cd16851c2cce471fbad563d33122264cc
Riyan Gendut <149429145+riyan-gendut@users.noreply.github.com>
2024-05-23 14:29:39 +0700

    Add files via upload

--- ffrecord.sh
+++ ffrecord.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 reslt="$1"
-echo "Currently Recording One Minute of Video"
-fmpeg -framerate 15 -re -f x11grab -s 1366x768 -thread_queue_size 1024 -t 01:00 -i :0.0 ~/Videos/"$reslt"".mp4"
+if [[ $# -lt 2 ]]; then
+	echo "Currently Recording One Minute of Video"
+	ffmpeg -framerate 15 -re -f x11grab -s 1366x768 -thread_queue_size 1024 -i :0.0 -t 01:00 -c:v libx264 ~/Videos/"$reslt"".mp4"
+else
+	audc="$2"
+	echo "Currently Recording One Minute of Video and Audio"
+	ffmpeg -framerate 15 -re -f x11grab -s 1366x768 -thread_queue_size 1024 -i :0.0 -f pulse -i $audc -ar 44100 -ac 1 -t 01:00 -c:v libx264 ~/Videos/"$reslt"".mp4"
+fi
