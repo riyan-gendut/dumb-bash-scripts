b4d1bcb8910ba1d7b2643186fba44bd880e0a55f
Riyan Gendut <149429145+riyan-gendut@users.noreply.github.com>
2024-12-28 11:37:01 +0700

    Update irresponsiblyrecode.sh

--- irresponsiblyrecode.sh
+++ irresponsiblyrecode.sh
@@ -2,8 +2,8 @@
 dewit(){
 #THIS WILL RECODE EVERY FILE IN CURRENT DIRECTORY AND ITS SUBDIRECTORY TO x265. USE AT YOUR OWN RISK.
 mkdir ~/vtout/
-find . -type f -name "*.mp4" -exec bash -c 'file="$1"; echo $(stat -c '%n' "$file") $(stat -c '%s' "$file"); ffmpeg -i "$file" -c:v libx265 -vtag hvc1 ~/vtout/"$file"' _ {} \;
-find . -type f -not -name "*.mp4" -exec bash -c 'file="$1"; echo $(stat -c '%n' "$file") $(stat -c '%s' "$file"); ffmpeg -i "$file" -c:v libx265 -vtag hvc1 ~/vtout/"$file".mp4' _ {} \;
+find . -type f -name "*.mp4" -exec bash -c 'file="$1"; echo $(stat -c '%n' "$file") $(stat -c '%s' "$file"); ffmpeg -n -i "$file" -c:v libx265 -vtag hvc1 ~/vtout/"$file"' _ {} \;
+find . -type f -not -name "*.mp4" -exec bash -c 'file="$1"; echo $(stat -c '%n' "$file") $(stat -c '%s' "$file"); ffmpeg -n -i "$file" -c:v libx265 -vtag hvc1 ~/vtout/"$file".mp4' _ {} \;
 }
 
 echo "THIS WILL TRANSCODE EVERY FILE IN CURRENT DIRECTORY AND ITS SUBDIRECTORY/IES TO x265. USE AT YOUR OWN RISK."
