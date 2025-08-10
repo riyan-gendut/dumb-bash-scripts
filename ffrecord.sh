ac4ab147045189117a1eb097fe1d5c45ee33073a
Riyan Gendut <149429145+riyan-gendut@users.noreply.github.com>
2025-08-11 05:46:16 +0700

    Add files via upload
    add preset flag

--- ffrecord.sh
+++ ffrecord.sh
@@ -2,9 +2,9 @@
 reslt="$1"
 if [[ $# -lt 2 ]]; then
 	echo "Currently Recording One Minute of Video"
-	ffmpeg -framerate 15 -re -f x11grab -s 1366x768 -thread_queue_size 1024 -i :0.0 -t 01:00 -c:v libx264 ~/Videos/"$reslt"".mp4"
+	ffmpeg -framerate 30 -re -f x11grab -s 1366x768 -thread_queue_size 1024 -i :0.0 -t 01:00 -c:v libx264 -preset ultrafast ~/Videos/"$reslt"".mp4"
 else
 	audc="$2"
 	echo "Currently Recording One Minute of Video and Audio"
-	ffmpeg -framerate 15 -re -f x11grab -s 1366x768 -thread_queue_size 1024 -i :0.0 -f pulse -i $audc -ar 44100 -ac 1 -t 01:00 -c:v libx264 ~/Videos/"$reslt"".mp4"
+	ffmpeg -framerate 15 -re -f x11grab -s 1366x768 -thread_queue_size 1024 -i :0.0 -f pulse -i $audc -ar 44100 -ac 1 -t 01:00 -c:v libx264 -preset ultrafast -tune zerolatency ~/Videos/"$reslt"".mp4"
 fi
